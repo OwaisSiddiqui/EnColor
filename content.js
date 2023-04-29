405a36275ac4c4c0547cff8049eaa1b096a9630d
= <=>
2023-04-29 14:50:30 -0400

    Update content

--- content.js
+++ content.js
@@ -2,15 +2,7 @@ new p5();
 
 const allElements = document.querySelectorAll("*")
 
-allElements.forEach((element) => {
-  const style = getComputedStyle(element);
-  const elementColor = color(style.color);
-  
-  const h = hue(elementColor);
-  const s = saturation(elementColor);
-  const l = lightness(elementColor);
-
-  function absColor(currVal, addedVal) {
+function absColor(currVal, addedVal) {
   if (currVal + addedVal > 255) {
     return (currVal + addedVal - 255)
   } else if 
@@ -20,9 +12,26 @@ allElements.forEach((element) => {
     else {
       return (currVal + addedVal)
     }
-  }
+}
 
+allElements.forEach((element) => {
+  colorMode(HSB, 255)
+  const style = getComputedStyle(element);
+  const elementColor = color(style.color);
+
+  const h = hue(elementColor);
+  const s = saturation(elementColor);
+  const b = brightness(elementColor);
   
-  const newHSLColor = `hsl(${constrain(absColor(h, -170), 21, 94)}, ${constrain(absColor(s, -50), 92, 189)}%, ${constrain(l, 85, 215)}%)`;
+  const newH = constrain(absColor(h, -170), 21, 94);
+  const newS = constrain(absColor(s, -50), 92, 189);
+  const newB = constrain(b, 85, 215);
+
+  const newColor = color(newH, newS, newB)
+
+  colorMode(HSL, 255)
+  
+  const newHSLColor = `hsl(${hue(newColor)}, ${saturation(newColor)}%, ${lightness(newColor)}%)`;
   element.style.color = newHSLColor;
 });
+
