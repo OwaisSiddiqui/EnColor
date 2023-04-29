a54dedc3ab677554cded7581c31030e5136c2477
JwuCode <jerrywu2390@gmail.com>
2023-04-29 14:13:46 -0400

    Implemented HSL Color Variance

--- content.js
+++ content.js
@@ -10,7 +10,19 @@ allElements.forEach((element) => {
   const s = saturation(elementColor);
   const l = lightness(elementColor);
 
-  const newHSLColor = `hsl(${h}, ${constrain(s + 300, 0, 100)}%, ${l}%)`;
+  function absColor(currVal, addedVal) {
+  if (currVal + addedVal > 255) {
+    return (currVal + addedVal - 255)
+  } else if 
+    (currVal + addedVal < 0) {
+      return (currVal + addedVal + 255)
+    }
+    else {
+      return (currVal + addedVal)
+    }
+  }
 
+  
+  const newHSLColor = `hsl(${constrain(absColor(h, -170), 21, 94)}, ${constrain(absColor(s, -50), 92, 189)}%, ${constrain(l, 85, 215)}%)`;
   element.style.color = newHSLColor;
 });
