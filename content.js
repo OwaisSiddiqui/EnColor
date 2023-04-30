aa5e8237197d47879ccfa032b466024dc6fb6bd7
= <=>
2023-04-29 22:08:20 -0400

    Update content

--- content.js
+++ content.js
@@ -1,86 +1,100 @@
 new p5();
-console.log("Working")
-const allElements = document.querySelectorAll("*")
+console.log("Working");
+const allElements = document.querySelectorAll("*");
 
 function absColor(currVal, addedVal) {
   if (currVal + addedVal > 255) {
-    return (currVal + addedVal - 255)
-  } else if 
-    (currVal + addedVal < 0) {
-      return (currVal + addedVal + 255)
-    }
-    else {
-      return (currVal + addedVal)
-    }
+    return currVal + addedVal - 255;
+  } else if (currVal + addedVal < 0) {
+    return currVal + addedVal + 255;
+  } else {
+    return currVal + addedVal;
+  }
 }
 
-function changeColor(element, url){
-  fetch(url)
-    .then(async (response)=>{
-        let imgData = await response.blob()
-        let fileData = new FormData()
-        let fileName = url.replaceAll("/", "")
-        fileData.append(fileName, imgData)
-        fetch('http://127.0.0.1:5000/convert', {method:'POST', 
-  mode: 'cors',
-body:fileData}).then(async(response)=>{
-    let blob = await response.blob()
-    var reader = new FileReader();
-reader.readAsDataURL(blob); 
-reader.onloadend = function() {
-  var base64data = reader.result;                
-  element.src = base64data
+function changeColor(element, url) {
+  fetch(url).then(async (response) => {
+    let imgData = await response.blob();
+    let fileData = new FormData();
+    let fileName = url.replaceAll("/", "");
+    fileData.append(fileName, imgData);
+    fetch("http://127.0.0.1:5000/convert", {
+      method: "POST",
+      mode: "cors",
+      body: fileData,
+    }).then(async (response) => {
+      let blob = await response.blob();
+      var reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = function () {
+        var base64data = reader.result;
+        element.src = base64data;
+      };
+    });
+  });
 }
-})
-    })
-}
-function restrict(val, lower, upper){
-if (val <= upper && val >= lower) {
-  return val
-} else {
-  return -1
-}
+function restrict(val, lower, upper) {
+  if (val <= upper && val >= lower) {
+    return val;
+  } else {
+    return -1;
+  }
 }
 
 allElements.forEach((element) => {
-  if (element.nodeName === 'IMG'){
-    changeColor(element, element.src)
-  }
-  else{
+  const properties = ["color", "backgroundColor"];
 
-  colorMode(HSB)
-  const style = getComputedStyle(element);
-  const elementColor = color(style.color);
+  properties.forEach((property) => {
+    if (
+      property === "backgroundColor" &&
+      element.nodeName.toLowerCase() === "p"
+    ) {
+      return;
+    }
+    if (element.nodeName === "IMG") {
+      changeColor(element, element.src);
+    } else {
+      colorMode(HSB);
+      const style = getComputedStyle(element);
+      const elementColor = color(style[property]);
 
-  const h = hue(elementColor);
-  const s = saturation(elementColor);
-  const b = brightness(elementColor);
+      const h = hue(elementColor);
+      const s = saturation(elementColor);
+      const b = brightness(elementColor);
 
-  console.log(element, style.color)
-  console.log(h, s, b)
+      console.log(element, style.color);
+      console.log(h, s, b);
 
-  var newH = h;
-  var newS = s;
-  var newB = b;
- 
-  if (restrict(h, 19, 130) != -1 && restrict(s, 46, 203) != -1 && restrict(b, 60, 194) != -1) {
-    newH = absColor(h,-170);
-    newS = 120
-    newB = b;
-    console.log("blind detected")
-  }
+      var newH = h;
+      var newS = s;
+      var newB = b;
 
-  console.log(getComputedStyle(element).color + " " + element.nodeName)
+      if (
+        restrict(h, 19, 130) != -1 &&
+        restrict(s, 46, 203) != -1 &&
+        restrict(b, 60, 194) != -1
+      ) {
+        newH = absColor(h, -170);
+        newS = 120;
+        newB = b;
+        console.log("blind detected");
 
-  console.log("New color 1", newH, newS, newB)
+        console.log(getComputedStyle(element).color + " " + element.nodeName);
 
-  const newColor = color(newH, newS, newB)
+        console.log("New color 1", newH, newS, newB);
 
-  colorMode(HSL)
+        const newColor = color(newH, newS, newB);
 
-  console.log("New color", newColor.toString("hsb"))
-  
-  const newHSLColor = `hsl(${hue(newColor)}, ${saturation(newColor)}%, ${lightness(newColor)}%)`;
-  element.style.color = newHSLColor;
-}
+        colorMode(HSL);
+
+        console.log("New color", newColor.toString("hsb"));
+
+        const newHSLColor = `hsl(${hue(newColor)}, ${saturation(
+          newColor
+        )}%, ${lightness(newColor)}%)`;
+
+        element.style[property] = newHSLColor;
+      }
+    }
+  });
 });
